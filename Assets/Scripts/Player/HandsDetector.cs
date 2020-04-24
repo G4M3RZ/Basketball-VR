7160a26084fafd9418adb9f099e6edc2216155be
Pablo <pablo.0698@hotmail.com>
2020-04-24 17:39:50 -0500

    Proyecto creado + gitignore

---
+++ Assets/Scripts/Player/HandsDetector.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsDetector : MonoBehaviour
+{
+    private GameObject _seleccion;
+    private Rigidbody _ballRgb;
+    private PlayerBody _pb;
+    private bool _isGrabed;
+
+    [Range(0,50)]
+    public float _frontForce, _upForce;
+
+    public Material[] _ball;
+
+    private void Start()
+    {
+        _pb = GetComponentInParent<PlayerBody>();
+    }
+    private void Update()
+    {
+        CathBall();
+ 
+        //cambiar forma de tirar
+        if(Input.GetKeyDown(KeyCode.R) && _isGrabed && _seleccion.transform.localPosition == Vector3.zero)
+            DropBall();
+    }
+    void CathBall()
+    {
+        if (_seleccion != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !_isGrabed)  //cambiar forma de agarrar
+            {
+                _ballRgb = _seleccion.GetComponent<Rigidbody>();
+                _seleccion.transform.parent = transform;
+                _isGrabed = true;
+            }
+        }
+
+        if (_isGrabed)
+        {
+            _ballRgb.constraints = RigidbodyConstraints.FreezeAll;
+            _seleccion.transform.localRotation = Quaternion.Euler(0, -90, 0);
+            _seleccion.transform.localPosition = Vector3.Lerp(_seleccion.transform.localPosition, Vector3.zero, Time.deltaTime * 10);
+
+            if (_pb._isUp)
+            {
+                for (int i = 0; i < _ball.Length; i++)
+                {
+                    
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _ball.Length; i++)
+                {
+
+                }
+            }
+        }
+    }
+    void DropBall()
+    {
+        _seleccion.transform.parent = null;
+        _seleccion = null;
+
+        _ballRgb.constraints = ~RigidbodyConstraints.FreezeAll;
+
+        if (_pb._isUp)
+        {
+            _ballRgb.AddForce(transform.forward * _frontForce * 10);
+            _ballRgb.AddForce(transform.up * _upForce * 20);
+        }
+        else
+        {
+            _ballRgb.AddForce(transform.forward * _frontForce * 10);
+        }
+
+        _ballRgb = null;
+        _isGrabed = false;
+    }
+
+    public void ObjectSelected(GameObject _obj)
+    {
+        if(!_isGrabed)
+            _seleccion = _obj;
+    }
+    public void ObjectDeselect()
+    {
+        if(!_isGrabed)
+            _seleccion = null;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBody.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBody : MonoBehaviour
+{
+    private GameObject _mainCam, _hands;
+    [Range(0,10)]
+    public float _highDistance;
+    [HideInInspector]
+    public bool _isUp;
+
+    private void Start()
+    {
+        _mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+        _hands = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        TurnBody();
+        HandsUpDown();
+    }
+
+    void TurnBody()
+    {
+        transform.rotation = Quaternion.Euler(0, _mainCam.transform.localEulerAngles.y, 0);
+    }
+    void HandsUpDown()
+    {
+        Vector3 startPos, endPos;
+
+        startPos = new Vector3(0, 0, _hands.transform.localPosition.z);
+        endPos = new Vector3(0, _highDistance, _hands.transform.localPosition.z);
+
+        if (_mainCam.transform.localEulerAngles.x - 360 <= -90)
+        {
+            _isUp = false;
+            _hands.transform.localPosition = Vector3.Lerp(_hands.transform.localPosition, startPos, Time.deltaTime * 5f);
+        }
+        else
+        {
+            _isUp = true;
+            _hands.transform.localPosition = Vector3.Lerp(_hands.transform.localPosition, endPos, Time.deltaTime * 5f);
+        }
+    }
+}
